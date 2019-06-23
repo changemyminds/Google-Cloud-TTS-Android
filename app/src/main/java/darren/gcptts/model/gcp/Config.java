ede6431dabec261b4d62813f580a0dc8007e590d
changemyminds <david7717318@gmail.com>
2019-06-24 00:10:28 +0800

    update API_KEY prompt

--- app/src/main/java/darren/gcptts/model/gcp/Config.java
+++ app/src/main/java/darren/gcptts/model/gcp/Config.java
@@ -7,7 +7,7 @@ package darren.gcptts.model.gcp;
  * Reference:
  */
 class Config {
-    static final String API_KEY = ""; // put your key to here
+    static final String API_KEY = "YOUR_API_KEY"; // put your key to here
     static final String API_KEY_HEADER = "X-Goog-Api-Key";
     static final String SYNTHESIZE_ENDPOINT = "https://texttospeech.googleapis.com/v1beta1/text:synthesize";
     static final String VOICES_ENDPOINT = "https://texttospeech.googleapis.com/v1beta1/voices";
